e5b53643e739543df0b48a4c7366d1383e9b0109
Kaidi Zhang <kaidi.zhang1@singtel.com>
2024-01-30 22:22:12 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `community-event`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sitemap"]
+};
